2b5f0ffc5d394348ea1b5a7a002f9d50f51eb3d7
User <golova92-69@yandex.ru>
2019-05-21 11:32:21 +0300

    change variables let to const

--- general.js
+++ general.js
@@ -1,17 +1,17 @@
-let newTodo = document.querySelector('.js-header__new-item');
-let todoList = document.querySelector('.js-todo-list');
-let firstLi = document.querySelectorAll('.js-todo-list__item')[0];
-let mainBlock = document.querySelector('.js-main');
-let footer = document.querySelector('.js-footer');
-let filters = document.querySelector('.js-filters');
-let btnClearCompleted = document.querySelector('.js-clear-completed');
-let btnToggleAll = document.querySelector('.js-select-all');
+const newTodo = document.querySelector('.js-header__new-item');
+const todoList = document.querySelector('.js-todo-list');
+const firstLi = document.querySelectorAll('.js-todo-list__item')[0];
+const mainBlock = document.querySelector('.js-main');
+const footer = document.querySelector('.js-footer');
+const filters = document.querySelector('.js-filters');
+const btnClearCompleted = document.querySelector('.js-clear-completed');
+const btnToggleAll = document.querySelector('.js-select-all');
 footer.hidden = true;
 mainBlock.hidden = true;
 btnClearCompleted.hidden = true;
 let arrayTodo = [];
 let count; 
-let filter = localStorage.getItem('filter');
+const filter = localStorage.getItem('filter');
 
 document.addEventListener('DOMContentLoaded', handleDomContentLoaded);
 function handleDomContentLoaded() {
@@ -25,8 +25,8 @@ function ready() {
 	if (arrayTodo && arrayTodo.length > 0) {
 		firstLi.hidden = false;
 		for (let i = 0; i < arrayTodo.length; i++) {
-			let newLi = firstLi.cloneNode(true);
-			let isComplete = arrayTodo[i].isComplete;
+			const newLi = firstLi.cloneNode(true);
+			const isComplete = arrayTodo[i].isComplete;
 			if (isComplete) newLi.children[0].children[0].checked = true;
 				newLi.setAttribute('id', arrayTodo[i].id );
 				newLi.querySelector('label').innerHTML = arrayTodo[i].text.trim();
@@ -74,8 +74,8 @@ function handleKeyDown(event) {
 function createNewItem() {
 	if ((event.keyCode === 13 || !event.keyCode) && newTodo.value !== '') {
 		firstLi.hidden = false;
-		let newLi = firstLi.cloneNode(true);
-		let obj = saveTodoItem(newTodo);
+		const newLi = firstLi.cloneNode(true);
+		const obj = saveTodoItem(newTodo);
 		newLi.setAttribute('id', obj.id);
 		newLi.querySelectorAll('label')[0].innerHTML = newTodo.value.trim();
 		todoList.appendChild(newLi);
@@ -90,8 +90,8 @@ function createNewItem() {
 
 function handleTodoListDblClick(event) {
 	if (event.target.localName === 'label') {
-		let labelValue = event.target.innerHTML;
-		let parentLi = event.target.parentNode.parentNode;
+		const labelValue = event.target.innerHTML;
+		const parentLi = event.target.parentNode.parentNode;
 		let newInput = document.createElement('INPUT');
 		parentLi.classList.add('js-editing');
 		newInput.className = 'js-edit';
@@ -104,10 +104,10 @@ function handleTodoListDblClick(event) {
 		function handleNewInputBlur(event) {
 			if (newInput.value !== '') {	
 				parentLi.classList.remove('js-editing');
-				let changeLabel = this.previousElementSibling.children[1];
+				const changeLabel = this.previousElementSibling.children[1];
 				changeLabel.innerHTML = newInput.value;
 				parentLi.removeChild(newInput);
-				let currentID = +parentLi.getAttribute('id');
+				const currentID = +parentLi.getAttribute('id');
 				for (let i = 0; i < arrayTodo.length; i++) {
 					if (currentID === arrayTodo[i].id) {
 						arrayTodo[i].text = changeLabel.innerHTML;
@@ -126,10 +126,10 @@ function handleTodoListDblClick(event) {
 				if (newInput.value !== '') {	
 					parentLi.classList.remove('js-editing');
 					if (event.type === 'keydown') newInput.removeEventListener('blur', handleNewInputBlur);
-					let changeLabel = this.previousElementSibling.children[1];
+					const changeLabel = this.previousElementSibling.children[1];
 					changeLabel.innerHTML = newInput.value;
 					parentLi.removeChild(newInput);
-					let currentID = +parentLi.getAttribute('id');
+					const currentID = +parentLi.getAttribute('id');
 					for (let i = 0; i < arrayTodo.length; i++) {
 						if (currentID === arrayTodo[i].id) {
 							arrayTodo[i].text = changeLabel.innerHTML;
@@ -152,7 +152,7 @@ function handleToggleAllClick(event) {
 	let complete = document.querySelectorAll('.complete');
 	if (event.target.checked) {
 		for (let i = 1; i < arrayTodo.length+1; i++) {
-		let parentLi = complete[i].parentNode.parentNode;
+		const parentLi = complete[i].parentNode.parentNode;
 			if (!complete[i].checked) {
 				complete[i].checked = true;
 				arrayTodo[i-1].isComplete = true;
@@ -163,7 +163,7 @@ function handleToggleAllClick(event) {
 	
 	} else  {
 		for (let i = 1; i < arrayTodo.length + 1; i++) {
-		let parentLi = complete[i].parentNode.parentNode;
+		 const parentLi = complete[i].parentNode.parentNode;
 			if (complete[i].checked) {
 				complete[i].checked = false;
 				arrayTodo[i-1].isComplete = false;
@@ -180,8 +180,8 @@ function handleToggleAllClick(event) {
 
 function handleTodoListClick(event) {
 	if (event.target.classList.contains('complete')) {
-		let parentLi = event.target.parentNode.parentNode;
-		let currentID = +parentLi.getAttribute('id');
+		const parentLi = event.target.parentNode.parentNode;
+		const currentID = +parentLi.getAttribute('id');
 			for (let i = 0; i < arrayTodo.length; i++) {
 				if (currentID === arrayTodo[i].id) {
 					if (event.target.checked) {
@@ -201,17 +201,17 @@ function handleTodoListClick(event) {
 		saveToStorage(); 
 	}
 	if (event.target.classList.contains('destroy')) {
-		let parentLi = event.target.parentNode.parentNode;
+		const parentLi = event.target.parentNode.parentNode;
 		removeTodoItem(parentLi);
 	}
 }
 
 function handleClearCompletedClick(event) {
-	let totalCount = arrayTodo.length;
+	const totalCount = arrayTodo.length;
 	let complete = document.querySelectorAll('.complete');
 		for (let i = 0; i <= totalCount; i++) {
 			if (complete[i].checked) {
-				let parentLi = complete[i].parentNode.parentNode;
+				const parentLi = complete[i].parentNode.parentNode;
 				removeTodoItem(parentLi);
 			}
 		}
@@ -226,7 +226,7 @@ function handleFiltersClick(event) {
 };
 
 function removeTodoItem(removeLi) {
-	let currentID = +removeLi.getAttribute('id');
+	const currentID = +removeLi.getAttribute('id');
 		for (let i = 0; i < arrayTodo.length; i++) {
 			if (currentID === arrayTodo[i].id) {
 				arrayTodo.splice(i, 1);
@@ -260,7 +260,7 @@ function saveToStorage() {
 };
 
 function showHideLi(currentLi, check) {
-	let filtersNodeList = document.querySelectorAll('.filter');
+	const filtersNodeList = document.querySelectorAll('.filter');
 	for (let i = 0; i < filtersNodeList.length; i++) {
 		if (filtersNodeList[i].checked) {
 			if (filtersNodeList[i].id === 'completed') {
@@ -274,7 +274,7 @@ function showHideLi(currentLi, check) {
 };
 
 function getInstalledFilterName() {
-	let filtersNodeList = document.querySelectorAll('.filter');
+	const filtersNodeList = document.querySelectorAll('.filter');
 	let filterName = '';
 	for (let i = 0; i < filtersNodeList.length; i++) {
 		if (filtersNodeList[i].checked) {
@@ -288,7 +288,7 @@ function setFilter(filterName) {
 	
 	if (filterName === 'active') {
 		for (let i = 0; i < todoList.childElementCount-1; i++) {
-			let checked = arrayTodo[i].isComplete;
+			const checked = arrayTodo[i].isComplete;
 			if (!checked) {
 				todoList.children[i+1].hidden = checked;
 			} else todoList.children[i+1].hidden = checked;
@@ -297,7 +297,7 @@ function setFilter(filterName) {
 
 	else if (filterName === 'completed') {
 		for (let i = 0; i < todoList.childElementCount-1; i++) {
-			let checked = arrayTodo[i].isComplete;
+			const checked = arrayTodo[i].isComplete;
 			if (checked) {
 				todoList.children[i+1].hidden = !checked;
 			} else todoList.children[i+1].hidden = !checked;
@@ -314,12 +314,12 @@ function setFilter(filterName) {
 }
 
 function initCounter () {  
-	let count = {
+	const count = {
 		total: arrayTodo.length,
 		active: arrayTodo.filter((i) => !i.isComplete).length,
 		completed: arrayTodo.filter((i) => i.isComplete ).length 
 		};
-	let labelCounter = document.querySelector('strong');
+	const labelCounter = document.querySelector('strong');
 	let itemsTextContent = '';
 	btnClearCompleted.hidden = count.completed <= 0;
 	count.active > 1 ? itemsTextContent = ' items left' : itemsTextContent =' item left';
@@ -330,9 +330,9 @@ function initCounter () {
 
 function updateCounter(parentLi, remove, add) {
 	let currentCount = count;
-	let labelCounter = document.querySelector('strong');
+	const labelCounter = document.querySelector('strong');
 	let itemsTextContent = '';
-	let isComplete = parentLi.querySelector('.complete').checked ? true: false;
+	const isComplete = parentLi.querySelector('.complete').checked;
 	if (remove) {
 		if (isComplete) {
 			currentCount.completed--;
